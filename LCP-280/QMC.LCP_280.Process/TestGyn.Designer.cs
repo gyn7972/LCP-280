7a529f77d58fdfe3487a9a824c47eea81f24cf13
yngoo <yngoo@iqmc.co.kr>
2025-08-25 09:02:34 +0900

    VisionImageViewer 추가 및 리소스 관리 개선
    QMC.Common.csproj에 VisionImageViewer.cs 파일을 추가하고, 관련 파일들을 포함시켰습니다.
    Resources.Designer.cs의 네임스페이스와 버전을 업데이트하여 리소스 관리를 개선했습니다.
    Resources.resx에서 XML 스키마를 수정하고 버전 정보를 4.0.0.0으로 변경했습니다.
    QMC.LCP_280.Process.csproj에 DesignTime 속성을 추가하여 디자인 타임 지원을 강화했습니다.
    TestGyn.Designer.cs에서 PictureBox를 VisionImageViewer로 변경하여 사용자 정의 이미지 뷰어를 사용하도록 했습니다.
    AbbreviationAttribute.cs 파일을 추가하여 열거형에 대한 약어 속성을 정의했습니다.
    VisionImageViewer.cs 파일을 새로 추가하여 이미지 뷰어 기능을 구현하고, megaphone_80px.png 아이콘을 추가했습니다.

--- LCP-280/QMC.LCP_280.Process/TestGyn.Designer.cs
+++ LCP-280/QMC.LCP_280.Process/TestGyn.Designer.cs
@@ -36,8 +36,35 @@
             this.labelDist = new System.Windows.Forms.Label();
             this.button_TestGyn_Test = new System.Windows.Forms.Button();
             this.button_TestGyn_Alarm = new System.Windows.Forms.Button();
+            //this.pictureBox_ImageView_Test = new System.Windows.Forms.PictureBox();
+            this.pictureBox_ImageView_Test = new QMC.Common.Vision.VisionImageViewer();
             ((System.ComponentModel.ISupportInitialize)(this.numDist)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ImageView_Test)).BeginInit();
             this.SuspendLayout();
+
+            // 
+            // pictureBox_ImageView_Test
+            // 
+            this.pictureBox_ImageView_Test.BackColor = System.Drawing.Color.Black;
+            this.pictureBox_ImageView_Test.Camera = null;
+            this.pictureBox_ImageView_Test.CameraSwitch = null;
+            this.pictureBox_ImageView_Test.FrameRate = 1D;
+            this.pictureBox_ImageView_Test.InputImage = null;
+            this.pictureBox_ImageView_Test.IsViewCustomizedImage = false;
+            this.pictureBox_ImageView_Test.Location = new System.Drawing.Point(6, 18);
+            this.pictureBox_ImageView_Test.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox_ImageView_Test.Name = "ImageViewer_Main_highs";
+            this.pictureBox_ImageView_Test.OperatingType = QMC.Common.Vision.VisionImageViewer.OperatingTypes.Center;
+            this.pictureBox_ImageView_Test.Simulated = false;
+            this.pictureBox_ImageView_Test.Size = new System.Drawing.Size(317, 265);
+            this.pictureBox_ImageView_Test.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_ImageView_Test.TabIndex = 189;
+            this.pictureBox_ImageView_Test.TabStop = false;
+            this.pictureBox_ImageView_Test.UpdateDelayTime = 200;
+            this.pictureBox_ImageView_Test.VisibleCrossLine = true;
+
+
+
             // 
             // comboUnit
             // 
@@ -128,11 +155,21 @@
             this.button_TestGyn_Alarm.UseVisualStyleBackColor = true;
             this.button_TestGyn_Alarm.Click += new System.EventHandler(this.button_TestGyn_Alarm_Click);
             // 
+            // pictureBox_ImageView_Test
+            // 
+            this.pictureBox_ImageView_Test.BackColor = System.Drawing.Color.Black;
+            this.pictureBox_ImageView_Test.Location = new System.Drawing.Point(438, 18);
+            this.pictureBox_ImageView_Test.Name = "pictureBox_ImageView_Test";
+            this.pictureBox_ImageView_Test.Size = new System.Drawing.Size(182, 248);
+            this.pictureBox_ImageView_Test.TabIndex = 10;
+            this.pictureBox_ImageView_Test.TabStop = false;
+            // 
             // TestGyn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox_ImageView_Test);
             this.Controls.Add(this.button_TestGyn_Alarm);
             this.Controls.Add(this.labelDist);
             this.Controls.Add(this.labelAxis);
@@ -145,6 +182,7 @@
             this.Text = "TestGyn";
             this.Load += new System.EventHandler(this.TestGyn_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numDist)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ImageView_Test)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,5 +198,7 @@
         private System.Windows.Forms.Label labelDist;
         private System.Windows.Forms.Button button_TestGyn_Test;
         private System.Windows.Forms.Button button_TestGyn_Alarm;
+        //private System.Windows.Forms.PictureBox pictureBox_ImageView_Test;
+        private QMC.Common.Vision.VisionImageViewer pictureBox_ImageView_Test;
     }
 }
