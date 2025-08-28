7fed97f8d5c80e50cba98227449586cd9da8baab
민홍 박 <mhpark@iqmc.co.kr>
2025-08-28 22:59:49 +0900

    LCP

--- LCP-280/QMC.Common/Motions/IPropertyCorrection.cs
+++ LCP-280/QMC.Common/Motions/IPropertyCorrection.cs
@@ -31,12 +31,17 @@ namespace QMC.Common.Motions
 
         public double ToHardware(double logicalValue)
         {
-            var v = logicalValue * _config.LogicalScaleFactor + _config.Offset;
-            return v * _setup.PulsesPerUnit;
+            var v = logicalValue;
+            return v;
+
+            //var v = logicalValue * _config.LogicalScaleFactor + _config.Offset;
+            //return v * _setup.PulsesPerUnit;
         }
 
         public double ToLogical(double hardwareValue)
         {
+            //var v = hardwareValue;
+            //return v;
             var v = hardwareValue / _setup.PulsesPerUnit;
             return (v - _config.Offset) / _config.LogicalScaleFactor;
         }
