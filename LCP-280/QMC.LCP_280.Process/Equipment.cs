b7000d34162fa74add83a01fe5052ccfdaf292a7
bhlee <bhlee@iqmc.co.kr>
2025-09-07 13:53:09 +0900

    AjinDriver.cs 홈 설정 개선 및 매개변수 적용
    AjinDriver.cs 파일에서 MotionAxisSetup 및 MotionAxisConfig 필드를 추가하고, 홈 설정 및 구성을 위한 로직을 개선했습니다. 홈 모드에서 속도 및 가속도를 설정하는 기능이 추가되었으며, 최근 설정을 캐시하여 재사용할 수 있도록 변경되었습니다. SCurve 프로파일 제어에 대한 조정도 포함되었습니다. Equipment.cs 파일에서는 모든 축에 대한 모션 매개변수를 적용하는 로직이 활성화되었습니다.

--- LCP-280/QMC.LCP_280.Process/Equipment.cs
+++ LCP-280/QMC.LCP_280.Process/Equipment.cs
@@ -981,7 +981,7 @@ namespace QMC.LCP_280.Process
 
             ClearAllMotionAlarmsOnStartup();
             ServoOnAllAxesOnStartup();
-            //ApplyMotionParamsForAllAxes(); 테스트중
+            ApplyMotionParamsForAllAxes();
 
             var scanner = new MotionStatusScanner(_axisManager, periodMs: 20);
             scanner.AxisStatusUpdated += (axis, status) =>
