915d4420a8d5f7b678d6d27c655cb6a3323fc077
gdgoo <gdgoo@iqmc.co.kr>
2025-09-23 20:04:18 +0900

    gdgoo
    ARM64/x64/x86 지원 및 새로운 컨트롤 추가
    
    ARM64, x64, x86 플랫폼에 대한 Debug/Release 구성 추가.
    `.editorconfig` 파일 추가로 코딩 스타일 및 서식 규칙 정의.
    새로운 UserControl(`DieOutputControl`, `DieIndexSelectControl` 등) 구현.
    공용 DisplayView 컨트롤 추가 및 렌더링/이벤트 처리 개선.
    UI 레이아웃 조정 및 샘플 데이터 초기화 코드 추가.
    다양한 리소스 파일 및 디자이너 파일 추가.
    
    ARM64/x64/x86 지원 및 새로운 UI 컨트롤 추가
    
    ARM64, x64, x86 플랫폼에 대한 Debug/Release 구성 추가.
    `.editorconfig` 파일을 추가하여 코딩 스타일 규칙 정의.
    `DieIndexSelectControl`, `DieOutputControl` 등 새로운 UserControl 추가.
    `DisplayView`에 툴팁, 호버 이벤트, 줌/드래그 기능 추가.
    `Monitoring_Main` 및 관련 UI 레이아웃 업데이트.
    실시간 데이터 시뮬레이션 및 차트 업데이트 로직 구현.
    프로젝트 확장성과 유지보수성을 위한 코드 구조 개선.

---
+++ LCP-280/QMC.LCP_280.Process/Unit/FormMain/InputWaferCarrierControl.cs
@@ -0,0 +1,64 @@
+﻿using QMC.LCP_280.Process.Work;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace QMC.LCP_280.Process.Unit.FormMain
+{
+    public partial class InputWaferCarrierControl : UserControl
+    {
+        private InputCassetteLifter CassetteLifter;
+        private InputFeeder Feeder;
+        private InputStage Stage;
+
+        public InputWaferCarrierControl() : this(
+            TryGetUnit<InputCassetteLifter>("InputCassetteLifter"),
+            TryGetUnit<InputFeeder>("InputFeeder"),
+            TryGetUnit<InputStage>("InputStage")
+            )
+        {
+
+        }
+
+        public InputWaferCarrierControl(InputCassetteLifter cassetteLifter, InputFeeder ringTransfer, InputStage inputStage)
+        {
+            InitializeComponent();
+
+            CassetteLifter = cassetteLifter;
+            Feeder = ringTransfer;
+            Stage = inputStage;
+
+            var materialCassette = CassetteLifter.GetMaterialCassette();
+            waferMapView.SetMaterialCassette(materialCassette);
+        }
+
+        private static T TryGetUnit<T>(string unitName) where T : class
+        {
+            try
+            {
+                var eq = Equipment.Instance;
+                if (eq?.Units != null && eq.Units.TryGetValue(unitName, out var u))
+                    return u as T;
+            }
+            catch { }
+            return null;
+        }
+
+        public void SetWaferCarrierId(string id)
+        {
+            lblWaferIdValue.Text = id;
+        }
+
+        public void UpdateWaferCount(int count)
+        {
+            lblWaferCountValue.Text = count.ToString();
+        }
+    }
+}
--- LCP-280/QMC.LCP_280.Process/Unit/FormMain/Monitoring_Main.Designer.cs
+++ LCP-280/QMC.LCP_280.Process/Unit/FormMain/Monitoring_Main.Designer.cs
@@ -24,91 +24,182 @@ namespace QMC.LCP_280.Process
         {
             this.groupBoxdieInputControl = new System.Windows.Forms.GroupBox();
             this.dieInputControl1 = new QMC.LCP_280.Process.Unit.FormMain.DieInputControl();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.groupBox6 = new System.Windows.Forms.GroupBox();
+
+            this.groupBoxdieIndexSelectControl = new System.Windows.Forms.GroupBox();
+            this.dieIndexSelectControl1 = new QMC.LCP_280.Process.Unit.FormMain.DieIndexSelectControl();
+
+            this.groupBoxdieOutputControl = new System.Windows.Forms.GroupBox();
+            this.dieOutputControl1 = new QMC.LCP_280.Process.Unit.FormMain.DieOutputControl();
+
+            this.groupBoxInputWaferCarrierControl = new System.Windows.Forms.GroupBox();
+            this.inputWaferCarrierControl1 = new QMC.LCP_280.Process.Unit.FormMain.InputWaferCarrierControl();
+
+            this.groupBoxMeasurementControl = new System.Windows.Forms.GroupBox();
+            this.measurementControl1 = new QMC.LCP_280.Process.Unit.FormMain.MeasurementControl();
+
+            this.groupBoxOutputWaferCarrierControl = new System.Windows.Forms.GroupBox();
+            this.outputWaferCarrierControl1 = new QMC.LCP_280.Process.Unit.FormMain.OutputWaferCarrierControl();
+
             this.groupBoxdieInputControl.SuspendLayout();
+            this.groupBoxdieIndexSelectControl.SuspendLayout();
+            this.groupBoxdieOutputControl.SuspendLayout();
+            this.groupBoxInputWaferCarrierControl.SuspendLayout();
+            this.groupBoxMeasurementControl.SuspendLayout();
+            this.groupBoxOutputWaferCarrierControl.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxdieInputControl
             // 
             this.groupBoxdieInputControl.Controls.Add(this.dieInputControl1);
-            this.groupBoxdieInputControl.Location = new System.Drawing.Point(12, 138);
+            this.groupBoxdieInputControl.Location = new System.Drawing.Point(12, 43);
+            this.groupBoxdieInputControl.Margin = new System.Windows.Forms.Padding(2);
             this.groupBoxdieInputControl.Name = "groupBoxdieInputControl";
-            this.groupBoxdieInputControl.Size = new System.Drawing.Size(330, 405);
+            this.groupBoxdieInputControl.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBoxdieInputControl.Size = new System.Drawing.Size(320, 370);
             this.groupBoxdieInputControl.TabIndex = 0;
             this.groupBoxdieInputControl.TabStop = false;
+            this.groupBoxdieInputControl.Text = "Die Input Tr";
             // 
             // dieInputControl1
             // 
             this.dieInputControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dieInputControl1.Location = new System.Drawing.Point(3, 21);
+            this.dieInputControl1.Location = new System.Drawing.Point(2, 16);
+            this.dieInputControl1.Margin = new System.Windows.Forms.Padding(2);
             this.dieInputControl1.Name = "dieInputControl1";
-            this.dieInputControl1.Size = new System.Drawing.Size(324, 381);
+            this.dieInputControl1.Size = new System.Drawing.Size(316, 352);
             this.dieInputControl1.TabIndex = 0;
             // 
-            // groupBox2
+            // groupBoxdieIndexSelectControl
             // 
-            this.groupBox2.Location = new System.Drawing.Point(348, 138);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(330, 405);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.groupBoxdieIndexSelectControl.Controls.Add(this.dieIndexSelectControl1);
+            this.groupBoxdieIndexSelectControl.Location = new System.Drawing.Point(332, 43);
+            this.groupBoxdieIndexSelectControl.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBoxdieIndexSelectControl.Name = "groupBoxdieIndexSelectControl";
+            this.groupBoxdieIndexSelectControl.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBoxdieIndexSelectControl.Size = new System.Drawing.Size(320, 370);
+            this.groupBoxdieIndexSelectControl.TabIndex = 1;
+            this.groupBoxdieIndexSelectControl.TabStop = false;
+            this.groupBoxdieIndexSelectControl.Text = "Index";
             // 
-            // groupBox3
+            // dieIndexSelectControl1
             // 
-            this.groupBox3.Location = new System.Drawing.Point(684, 138);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(330, 405);
-            this.groupBox3.TabIndex = 2;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "groupBox3";
+            this.dieIndexSelectControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.dieIndexSelectControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dieIndexSelectControl1.Location = new System.Drawing.Point(2, 16);
+            this.dieIndexSelectControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.dieIndexSelectControl1.Name = "dieIndexSelectControl1";
+            this.dieIndexSelectControl1.Size = new System.Drawing.Size(316, 352);
+            this.dieIndexSelectControl1.TabIndex = 1;
             // 
-            // groupBox4
+            // groupBoxdieOutputControl
             // 
-            this.groupBox4.Location = new System.Drawing.Point(15, 549);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(330, 405);
-            this.groupBox4.TabIndex = 4;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "groupBox4";
+            this.groupBoxdieOutputControl.Controls.Add(this.dieOutputControl1);
+            this.groupBoxdieOutputControl.Location = new System.Drawing.Point(652, 43);
+            this.groupBoxdieOutputControl.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBoxdieOutputControl.Name = "groupBoxdieOutputControl";
+            this.groupBoxdieOutputControl.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBoxdieOutputControl.Size = new System.Drawing.Size(320, 370);
+            this.groupBoxdieOutputControl.TabIndex = 2;
+            this.groupBoxdieOutputControl.TabStop = false;
+            this.groupBoxdieOutputControl.Text = "Die Output Tr";
             // 
-            // groupBox5
+            // dieOutputControl1
             // 
-            this.groupBox5.Location = new System.Drawing.Point(348, 549);
-            this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(330, 405);
-            this.groupBox5.TabIndex = 3;
-            this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "groupBox5";
+            this.dieOutputControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dieOutputControl1.Location = new System.Drawing.Point(2, 16);
+            this.dieOutputControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.dieOutputControl1.Name = "dieOutputControl1";
+            this.dieOutputControl1.Size = new System.Drawing.Size(316, 352);
+            this.dieOutputControl1.TabIndex = 2;
             // 
-            // groupBox6
+            // groupBoxInputWaferCarrierControl
             // 
-            this.groupBox6.Location = new System.Drawing.Point(684, 549);
-            this.groupBox6.Name = "groupBox6";
-            this.groupBox6.Size = new System.Drawing.Size(330, 405);
-            this.groupBox6.TabIndex = 4;
-            this.groupBox6.TabStop = false;
-            this.groupBox6.Text = "groupBox6";
+            this.groupBoxInputWaferCarrierControl.Controls.Add(this.inputWaferCarrierControl1);
+            this.groupBoxInputWaferCarrierControl.Location = new System.Drawing.Point(12, 424);
+            this.groupBoxInputWaferCarrierControl.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBoxInputWaferCarrierControl.Name = "groupBoxInputWaferCarrierControl";
+            this.groupBoxInputWaferCarrierControl.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBoxInputWaferCarrierControl.Size = new System.Drawing.Size(320, 370);
+            this.groupBoxInputWaferCarrierControl.TabIndex = 3;
+            this.groupBoxInputWaferCarrierControl.TabStop = false;
+            this.groupBoxInputWaferCarrierControl.Text = "Input Wafer Carrier";
+            // 
+            // inputWaferCarrierControl1
+            // 
+            this.inputWaferCarrierControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputWaferCarrierControl1.Location = new System.Drawing.Point(2, 16);
+            this.inputWaferCarrierControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.inputWaferCarrierControl1.Name = "inputWaferCarrierControl1";
+            this.inputWaferCarrierControl1.Size = new System.Drawing.Size(316, 352);
+            this.inputWaferCarrierControl1.TabIndex = 4;
+
+            // 
+            // groupBoxMeasurementControl
+            // 
+            this.groupBoxMeasurementControl.Controls.Add(this.measurementControl1);
+            this.groupBoxMeasurementControl.Location = new System.Drawing.Point(332, 424);
+            this.groupBoxMeasurementControl.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBoxMeasurementControl.Name = "groupBoxMeasurementControl";
+            this.groupBoxMeasurementControl.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBoxMeasurementControl.Size = new System.Drawing.Size(320, 370);
+            this.groupBoxMeasurementControl.TabIndex = 4;
+            this.groupBoxMeasurementControl.TabStop = false;
+            this.groupBoxMeasurementControl.Text = "Measurement";
+            // 
+            // measurementControl1
+            // 
+            this.measurementControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.measurementControl1.Location = new System.Drawing.Point(2, 16);
+            this.measurementControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.measurementControl1.Name = "measurementControl1";
+            this.measurementControl1.Size = new System.Drawing.Size(316, 352);
+            this.measurementControl1.TabIndex = 4;
+
+            // 
+            // groupBoxOutputWaferCarrierControl
+            // 
+            this.groupBoxOutputWaferCarrierControl.Controls.Add(this.outputWaferCarrierControl1);
+            this.groupBoxOutputWaferCarrierControl.Location = new System.Drawing.Point(652, 424);
+            this.groupBoxOutputWaferCarrierControl.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBoxOutputWaferCarrierControl.Name = "groupBoxOutputWaferCarrierControl";
+            this.groupBoxOutputWaferCarrierControl.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBoxOutputWaferCarrierControl.Size = new System.Drawing.Size(320, 370);
+            this.groupBoxOutputWaferCarrierControl.TabIndex = 3;
+            this.groupBoxOutputWaferCarrierControl.TabStop = false;
+            this.groupBoxOutputWaferCarrierControl.Text = "Input Wafer Carrier";
+            // 
+            // outputWaferCarrierControl1
+            // 
+            this.outputWaferCarrierControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputWaferCarrierControl1.Location = new System.Drawing.Point(2, 16);
+            this.outputWaferCarrierControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.outputWaferCarrierControl1.Name = "outputWaferCarrierControl1";
+            this.outputWaferCarrierControl1.Size = new System.Drawing.Size(316, 352);
+            this.outputWaferCarrierControl1.TabIndex = 4;
+
             // 
             // Monitoring_Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(1303, 1003);
-            this.Controls.Add(this.groupBox6);
-            this.Controls.Add(this.groupBox4);
-            this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox5);
-            this.Controls.Add(this.groupBox2);
+            this.ClientSize = new System.Drawing.Size(1042, 802);
+
             this.Controls.Add(this.groupBoxdieInputControl);
+            this.Controls.Add(this.groupBoxdieIndexSelectControl);
+            this.Controls.Add(this.groupBoxdieOutputControl);
+
+            this.Controls.Add(this.groupBoxInputWaferCarrierControl);
+            this.Controls.Add(this.groupBoxMeasurementControl);
+            this.Controls.Add(this.groupBoxOutputWaferCarrierControl);
+
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Monitoring_Main";
             this.Text = "Monitoring";
             this.Load += new System.EventHandler(this.Monitoring_Main_Load);
             this.groupBoxdieInputControl.ResumeLayout(false);
+            this.groupBoxdieIndexSelectControl.ResumeLayout(false);
+            this.groupBoxdieOutputControl.ResumeLayout(false);
+            this.groupBoxMeasurementControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -117,10 +208,20 @@ namespace QMC.LCP_280.Process
 
         private GroupBox groupBoxdieInputControl;
         private QMC.LCP_280.Process.Unit.FormMain.DieInputControl dieInputControl1;
-        private GroupBox groupBox2;
-        private GroupBox groupBox3;
-        private GroupBox groupBox4;
-        private GroupBox groupBox5;
-        private GroupBox groupBox6;
+
+        private GroupBox groupBoxdieIndexSelectControl;
+        private QMC.LCP_280.Process.Unit.FormMain.DieIndexSelectControl dieIndexSelectControl1;
+
+        private GroupBox groupBoxdieOutputControl;
+        private QMC.LCP_280.Process.Unit.FormMain.DieOutputControl dieOutputControl1;
+
+        private GroupBox groupBoxMeasurementControl;
+        private QMC.LCP_280.Process.Unit.FormMain.MeasurementControl measurementControl1;
+
+        private GroupBox groupBoxInputWaferCarrierControl;
+        private QMC.LCP_280.Process.Unit.FormMain.InputWaferCarrierControl inputWaferCarrierControl1;
+
+        private GroupBox groupBoxOutputWaferCarrierControl;
+        private QMC.LCP_280.Process.Unit.FormMain.OutputWaferCarrierControl outputWaferCarrierControl1;
     }
 }
