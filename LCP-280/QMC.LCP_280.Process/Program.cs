24df53ee4ba77edd67610e4904582620914403c8
김영남 <@DESKTOP-LRBAVKP>
2025-09-24 16:10:20 +0900

    예외 처리 및 미니 덤프 기능 추가
    - `MinidumpHelp.cs` 파일을 추가하여 프로그램에서 발생하는 예외를 처리하고 미니 덤프 파일을 생성하는 기능을 구현했습니다.
    - `Program.cs` 파일에 `Application.ThreadException` 이벤트를 통해 스레드 예외를 처리하는 `exceptionDump` 메서드를 등록했습니다.
    - `CurrentDomain_UnhandledException` 메서드를 추가하여 애플리케이션 도메인에서 처리되지 않은 예외를 처리하도록 했습니다.

--- LCP-280/QMC.LCP_280.Process/Program.cs
+++ LCP-280/QMC.LCP_280.Process/Program.cs
@@ -17,6 +17,16 @@ namespace QMC.LCP_280.Process
         {
             Log.Write("LCP_280", "Program Start--------------------.");
 
+
+
+
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(exceptionDump);
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
@@ -27,5 +37,29 @@ namespace QMC.LCP_280.Process
             
             Application.Run(new QMC.Common.MainForm()); // 정확한 네임스페이스 지정
         }
+
+        static void exceptionDump(object sender, System.Threading.ThreadExceptionEventArgs args)
+        {
+            //Exception e = args.Exception;
+            //Console.WriteLine("errMsg: " + e.Message);
+            //Console.WriteLine("errPos: " + e.TargetSite);
+
+            //덤프 파일 경로 설정(MinidumpHelp.cs 에서도 수정)
+            //MinidumpHelp.Minidump.install_self_mini_dump(Application.StartupPath);
+
+            MinidumpHelp.Minidump.install_self_mini_dump();
+        }
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            //Exception e = args.Exception;
+            //Console.WriteLine("errMsg: " + e.Message);
+            //Console.WriteLine("errPos: " + e.TargetSite);
+
+            //덤프 파일 경로 설정(MinidumpHelp.cs 에서도 수정)
+            //MinidumpHelp.Minidump.install_self_mini_dump(Application.StartupPath);
+
+            MinidumpHelp.Minidump.install_self_mini_dump();
+        }
+
     }
 }
